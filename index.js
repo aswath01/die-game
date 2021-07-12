f3d13f5a5968fa562ef294eb1872203b4d82a1da
aswath <aswath.710@gmail.com>
2021-07-12 17:49:12 +0530

    dom-commit

---
+++ index.js
@@ -0,0 +1,17 @@
+var a = Math.ceil(Math.random() * 6);
+var randomImage = "Images/dice" + a + ".png";
+var image = document.querySelectorAll("img")[0];
+image.setAttribute("src", randomImage);
+
+var b = Math.ceil(Math.random() * 6);
+var randommage = "Images/dice" + b + ".png";
+var image2 = document.querySelectorAll("img")[1];
+image2.setAttribute("src", randommage);
+
+if (a > b) {
+    document.querySelector("h1").innerHTML = "yayy! player1 won!";
+} else if (a < b) {
+    document.querySelector("h1").innerHTML = "yayy! player2 won!";
+} else {
+    document.querySelector("h1").innerHTML = "its a draw";
+}
